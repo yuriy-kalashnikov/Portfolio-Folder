f075d9adc98ab1017dbabc61ccceaa9ea46c11da
yuriy-kalashnikov <60500807+yuriy-kalashnikov@users.noreply.github.com>
2025-11-02 23:13:40 +0300

    Fix Build

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Hero } from "./components/Hero";
-import { Services } from "./components/Services";
-import { Experience } from "./components/Experience";
-import { Portfolio } from "./components/Portfolio";
-import { About } from "./components/About";
-import { Contact } from "./components/Contact";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Experience from "./components/Experience";
+import Portfolio from "./components/Portfolio";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 export default function App() {
   return (
